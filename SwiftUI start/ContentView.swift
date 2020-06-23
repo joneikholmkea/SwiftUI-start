95a7259ba386755af45f7d2152f86a9f4cfb9471
Jon Eikholm <jone@kea.dk>
2020-06-23 13:11:29 +0200

    first

---
+++ SwiftUI start/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI start
+//
+//  Created by Jon Eikholm on 23/06/2020.
+//  Copyright © 2020 Jon Eikholm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
